35313b978e24c5a9e5889a4f340f225eb30d0b9d
unknown <pablolappano@gmail.com>
2020-08-31 21:35:17 -0300

    ahi va de nuevoo

--- POO/clase_auto/clase_auto.ts
+++ POO/clase_auto/clase_auto.ts
@@ -1,61 +1,61 @@
-class Auto{
-    private velocidad:number
-    private marcha:number
-    private combustible:number
-    private encendido:boolean
+class Auto {
+    private velocidad: number;
+    private marcha: number;
+    private combustible: number;
+    private encendido: boolean;
 
-constructor(combustibleInicial:number) {
-    this.velocidad=0;
-    this.marcha=0;
-    this.combustible=combustibleInicial;
-    this.encendido=false
-}
-acelerar():void{ 
-    if (this.combustible > 0 && this.encendido==true){
-        this.velocidad=this.velocidad+10;
-        this.combustible--
-        }
-    if (this.combustible <=0){
-        this.encendido=false;
-        }
-}
-
-desacelerar():void{ 
-        if (this.velocidad > 10){
-            this.velocidad=this.velocidad-10;
-        }
-}
-
-subirMarcha():void{ 
-    if (this.marcha <= 4 && this.encendido==true){
-        this.marcha++
-        this.combustible--
+    public constructor(combustibleInicial: number) {
+        this.velocidad = 0;
+        this.marcha = 0;
+        this.combustible = combustibleInicial;
+        this.encendido = false;
     }
-    if (this.combustible <=0){
-        this.encendido=false;
-      }
-}
-
-bajarMarcha():void{ 
-    if (this.marcha >= 0 && this.encendido==true){
-        this.marcha--
-        this.combustible--
+    public acelerar(): void {
+        if (this.combustible > 0 && this.encendido == true) {
+            this.velocidad = this.velocidad + 10;
+            this.combustible--;
+        }
+        if (this.combustible <= 0) {
+            this.encendido = false;
+        }
     }
-    if (this.combustible <=0){
-        this.encendido=false;
-        
+
+    public desacelerar(): void {
+        if (this.velocidad > 10) {
+            this.velocidad = this.velocidad - 10;
+        }
     }
-}
 
-cargarCombustible(litros:number):void{
-    this.combustible=this.combustible+litros
-}
+    public subirMarcha(): void {
+        if (this.marcha <= 4 && this.encendido == true) {
+            this.marcha++;
+            this.combustible--;
+        }
+        if (this.combustible <= 0) {
+            this.encendido = false;
+        }
+    }
 
-encender(): void {
-    if(this.encendido)
-    this.encendido = false
+    public bajarMarcha(): void {
+        if (this.marcha >= 0 && this.encendido == true) {
+            this.marcha--;
+            this.combustible--;
+        }
+        if (this.combustible <= 0) {
+            this.encendido = false;
+
+        }
+    }
+
+    public cargarCombustible(litros: number): void {
+        this.combustible = this.combustible + litros;
+    }
+
+    public encender(): void {
+        if (this.encendido)
+            this.encendido = false;
         else
-            this.encendido = true
-}
+            this.encendido = true;
+    }
 }
 
